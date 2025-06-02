3117a247a0bccf740ed80d9e9c110568346f4817
B1zn3 <kuprianevgenij75@gmail.com>
2025-06-02 19:48:52 +0300

    Add new pages and update existing files

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+
 
 
 app = Flask(__name__)
+
+
 
 @app.route("/home/")
 def home():
     return render_template('index.html')
 
-@app.route("/<string:username>")
-def index(username):
-    return render_template('index.html', title='Главная', name=username)
+@app.route("/<int:id>")
+def id_1(id):
+    return render_template('about.html', username=id)
+
+@app.route("/about/<string:username>")
+def about(username):
+    return render_template('about.html', title = 'О нас', username=username)
+
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        FIO = request.form['Username']
+        TEL = request.form['TEL']
+        DATE_BIRTH = request.form['Password']
+        print(FIO, TEL, DATE_BIRTH)
+    return render_template('register.html', title='Форма регистрации')
+
 
 # with app.test_request_context():
 #     print(url_for('home'))
